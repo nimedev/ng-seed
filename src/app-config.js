5345034095be9efb3f0701e7454524ec1227bc70
nimedev <nicolasmejiaco@gmail.com>
2017-04-11 21:22:59 -0500

    Use Object.freeze() in app-config.

--- src/app-config.js
+++ src/app-config.js
@@ -3,10 +3,10 @@
  * @module app-config
  */
 
-export default {
+export default Object.freeze({
   // API base url
   REST_URL: process.env.NG_SEED_API_URL,
 
   // Save the environment
   ENV: process.env.NODE_ENV
-}
+})
